e6ff42891f1b44d1310504d05987869de1fb4e67
nataliekao03 <natkao3011@gmail.com>
2023-10-09 20:41:48 -0700

    fix q1

--- ladder.py
+++ ladder.py
@@ -2,5 +2,11 @@
 def my_steps(n):
     if n < 1 or n > 25:
         raise ValueError("Invalid input")
+    
+    if n ==1:
+        return 1
+    
+    if n==2:
+        return 2
 
     return my_steps(n - 1) + my_steps(n - 2)
