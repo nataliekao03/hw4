aa52ae6a2bd8834f7ae6b9b9d4be99bf77f0d4d3
nataliekao03 <natkao3011@gmail.com>
2023-10-09 20:06:30 -0700

    second commit

--- ladder.py
+++ ladder.py
@@ -1,6 +1,6 @@
 # to run program, go to terminal and run "python3 ladder.py"
 def my_steps(n):
     if n < 1 or n > 25:
-        return "Invalid input"
+        raise ValueError("Invalid input")
 
     return my_steps(n - 1) + my_steps(n - 2)
--- merge.py
+++ merge.py
@@ -1,4 +1,24 @@
 def merge_list(list1, list2):
-    newList = list1 + list2
-    newList.sort()  # n log n
-    return newList
+    if not(isinstance(list1, list) and isinstance(list2, list)):
+        raise TypeError("Inputs should be lists")
+    
+    for item in list1 + list2:
+        if not isinstance(item, int):
+            raise TypeError("Inputs can only contain integers")
+    
+    new_arr = []
+    i = 0
+    j = 0
+
+    while i < len(list1) and j < len(list2):
+       if list1[i] <  list2[j]:
+        new_arr.append(list1[i])
+        i += 1
+
+    else:
+        new_arr.append(list2[j])
+        j += 1
+
+    new_arr = new_arr + list1[i:] + list2[j:]
+
+    
