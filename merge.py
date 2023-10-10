446fff2d4cb7fdf942f275410af44b4bb79629e7
nataliekao03 <natkao3011@gmail.com>
2023-10-09 21:29:44 -0700

    new commit

--- merge.py
+++ merge.py
@@ -11,14 +11,19 @@ def merge_list(list1, list2):
     j = 0
 
     while i < len(list1) and j < len(list2):
-        if list1[i] <  list2[j]:
+        if list1[i] <= list2[j]:
             new_arr.append(list1[i])
             i += 1
         else:
             new_arr.append(list2[j])
             j += 1
 
-    new_arr = new_arr + list1[i:] + list2[j:]
+    new_arr.extend(list1[i: ])
+    new_arr.extend(list2[j: ])
+
     return new_arr
+
+
+
 
     
