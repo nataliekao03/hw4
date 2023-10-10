353aafc7c577504b4eeac6ece57fa953a9bd8527
nataliekao03 <natkao3011@gmail.com>
2023-10-09 22:10:04 -0700

    new commit

---
+++ bug1.py
@@ -0,0 +1,27 @@
+class Base:
+  def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+  def shape(self):
+        return "This is a shape"
+
+class Circle(Base):
+    def __init__(x, size):
+        super().__init__(x, y, size)
+
+    def draw(self):
+        return f"""
+({self.x}, {self.y})\n{self.size}
+         , - ~ ~ ~ - ,
+     , '               ' ,
+   ,                       ,
+  ,                         ,
+ ,                           ,
+ ,                           ,
+ ,                           ,
+  ,                         ,
+   ,                       ,
+     ,                  , '
+       ' - , _ _ _ ,  '
+               """
--- merge.py
+++ merge.py
@@ -16,6 +16,7 @@ def merge_list(list1, list2):
         else:
             new_arr.append(list2[j])
             j += 1
+
     new_arr.extend(list1[i:])  
     new_arr.extend(list2[j:]) 
     return new_arr
