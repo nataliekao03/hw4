5ea5462e8d4a6769cb2bb7caf4f3d6085446f692
nataliekao03 <natkao3011@gmail.com>
2023-10-10 18:25:32 -0700

    next commit

--- merge.py
+++ merge.py
@@ -1,4 +1,4 @@
-def merge(list1, list2):
+def merge_list(list1, list2):
     if not (isinstance(list1, list) and isinstance(list2, list)):
             raise TypeError("Inputs both have to be lists")
 
@@ -6,9 +6,9 @@ def merge(list1, list2):
         if not isinstance(item, int):
             raise TypeError("Input must contain only integers")
 
+    new_arr = []
     i=0 
     j=0
-    new_arr = []
     while i < len(list1) and j < len(list2):
         if list1[i] <= list2[j]:
             new_arr.append(list1[i])
@@ -24,7 +24,7 @@ def merge(list1, list2):
 
     else:
         while j < len(list2):
-            new_arr.append(second[j])
+            new_arr.append(list2[j])
             j += 1
 
     return new_arr
