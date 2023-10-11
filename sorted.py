038fd938b913fb5033e68d41bec8f4f3439ba306
nataliekao03 <natkao3011@gmail.com>
2023-10-10 23:56:58 -0700

    new commit

--- sorted.py
+++ sorted.py
@@ -1,25 +1,3 @@
 def sort_dictionary(input_dict):
-    keys = list(input_dict.keys())
-    vals = list(input_dict.values())
-
-    sorted_list = []
-    i = 0
-
-    while i < len(input_dict):
-        greatest = 0
-        j = 0
-
-        while j < len(keys):
-            if vals[j][1] >= vals[greatest][1]:
-                greatest = j
-
-            j += 1
-
-        sorted_list.append(
-            (keys[greatest], vals[greatest][0])
-        )  # Access the phone number (first element of the tuple)
-        keys.pop(greatest)
-        vals.pop(greatest)
-        i += 1
-
-    return sorted_list
+    sorted_list = sorted(input_dict.items(), key=lambda item: item[1][1], reverse=True)
+    return [(name, data[0]) for name, data in sorted_list]
