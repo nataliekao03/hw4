2f146a68df84d653a23a1278086572b909b3d069
nataliekao03 <natkao3011@gmail.com>
2023-10-10 20:39:21 -0700

    q3 commit

--- sorted.py
+++ sorted.py
@@ -1,22 +1,23 @@
-def sort_dictionary(dict):
-    keys = list(dict.keys())
-    vals = list(dict.values())
+def sort_dictionary(input_dict):
+    keys = list(input_dict.keys())
+    vals = list(input_dict.values())
 
-    sorted = []
+    sorted_list = []
     i = 0
-    j = 0
 
-    while i < len(dict):
+    while i < len(input_dict):
         greatest = 0
+        j = 0
+
         while j < len(keys):
-            if vals[greatest][1] > vals[j][1] :
+            if vals[j][1] > vals[greatest][1]:  # Change the comparison condition to sort in descending order
                 greatest = j
-            
+
             j += 1
-        sorted.append((keys[greatest], vals[greatest][0]))
-        keys.pop(greatest)   
-        vals.pop(greatest)   
+
+        sorted_list.append((keys[greatest], vals[greatest][0]))  # Access the phone number (first element of the tuple)
+        keys.pop(greatest)
+        vals.pop(greatest)
         i += 1
-        greatest = 0
-    
-    return sorted
+
+    return sorted_list
