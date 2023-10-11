0cc3eed692009c3211b6ce9cafc0a8f9453ec8c3
nataliekao03 <natkao3011@gmail.com>
2023-10-10 20:10:01 -0700

    new commit

--- sorted.py
+++ sorted.py
@@ -1,2 +1,22 @@
-def reverse_sort_dictionary(list1, list2):
-    return 0
+def sort_dictionary(dict):
+    keys = list(dict.keys())
+    vals = list(dict.values())
+
+    sorted = []
+    i = 0
+    j = 0
+    smallest = 0
+
+    while i < len(dict):
+        while j < len(keys):
+            if vals[smallest][1] > vals[j][1]:
+                smallest = j
+            
+            j += 1
+        sorted.append((keys[smallest], vals[smallest][0]))
+        del keys[smallest]
+        del vals[smallest]
+        i += 1
+        smallest = 0
+    
+    return sorted
