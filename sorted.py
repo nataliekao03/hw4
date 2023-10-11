1ab118c4bc49e68332c34d85fe390267b7665680
nataliekao03 <natkao3011@gmail.com>
2023-10-11 00:01:04 -0700

    update sorted.py

--- sorted.py
+++ sorted.py
@@ -1,3 +1,4 @@
 def sort_dictionary(input_dict):
     sorted_list = sorted(input_dict.items(), key=lambda item: item[1][1], reverse=True)
-    return [(name, data[0]) for name, data in sorted_list]
+    result = [(name, data[0] for name, data in sorted_list)]
+    return result
